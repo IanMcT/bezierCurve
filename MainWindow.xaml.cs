f42f2410cb1520e2af45ae3c80ed1e1bd44d2c37
IanMcT <ian.mctavish@tldsb.on.ca>
2018-03-19 13:50:16 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * I. McTavish
+ * BezierCurve
+ * March 19, 2018
+ * Creates a Bezier Curve in C# Code
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Drawing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Create the path with the geometry
+            Path p = new Path();
+            p.Stroke = Brushes.Red;
+            p.StrokeThickness = 15;
+            PathGeometry pg = new PathGeometry();
+            PathFigureCollection pfg = new PathFigureCollection();
+            PathFigure pathFigure = new PathFigure();
+            pathFigure.StartPoint = new Point(10, 100);
+            pathFigure.Segments = new PathSegmentCollection();
+
+            QuadraticBezierSegment quadraticBezierSegment = new QuadraticBezierSegment();
+            quadraticBezierSegment.Point1 = new Point(150, 300);
+            quadraticBezierSegment.Point2 = new Point(300, 100);
+            //everything gets put together
+            pathFigure.Segments.Add(quadraticBezierSegment);
+            pfg.Add(pathFigure);
+            pg.Figures = pfg;
+            p.Data = pg;
+            //add it to the canvas - xaml has a Canvas with the x:Name canvas
+            canvas.Children.Add(p);
+            
+        }
+    }
+}
